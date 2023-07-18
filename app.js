b454550b8e2a41feef679f44391dd38a5f9266ea
Nerro <nduguiskarioti@gmail.com>
2023-07-18 19:25:30 +0300

    configuration

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.post("/register", (req, res) => {
   //   "/images/profiles/" +
   //   req.file.filename;
   con.query(
-    "SELECT email FROM ravers WHERE email = ?",
+    "SELECT email FROM test1 WHERE email = ?",
     [req.body.email],
     (error, results) => {
       if (results.length > 0) {
@@ -77,7 +77,7 @@ app.post("/register", (req, res) => {
         if (req.body.password === req.body.confirm_password) {
           bcrypt.hash(req.body.password, 5, function (err, hash) {
             con.query(
-              "INSERT INTO ravers(name,phone,password,email) VALUES(?,?,?,?)",
+              "INSERT INTO test1(name,phone,password,email) VALUES(?,?,?,?)",
               [req.body.name, req.body.phone, hash, req.body.email],
               (error) => {
                 if (error) {
@@ -100,7 +100,7 @@ app.get("/login", (req, res) => {
 });
 app.post("/login", (req, res) => {
   con.query(
-    "SELECT * FROM ravers WHERE email = ?",
+    "SELECT * FROM test1 WHERE email = ?",
     [req.body.email],
     (error, user) => {
       if (error) {
